52c4d03aeb3d620d6ca0502367c5ce3162ec6186
Andoni Eguiluz <andoni.eguiluz@deusto.es>
2018-04-18 15:29:07 +0200

    Clase ventana

---
+++ Clase2018Prog2/src/tema07/VentanaUsuario.java
@@ -0,0 +1,21 @@
+package tema07;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class VentanaUsuario extends JFrame {
+	public VentanaUsuario() {
+		// 1.- Inicializar la ventana
+		setTitle( "Usuario 1" );
+		setSize( 320, 200 );
+		setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE);
+		// 2.- Crear sus componentes y contenedores
+		// 3.- Asociar componentes a contenedores
+		// 4.- Crear y asociar eventos a componentes
+	}
+
+	public static void main(String[] args) {
+		VentanaUsuario v = new VentanaUsuario();
+		v.setVisible( true );
+	}
+
+}
