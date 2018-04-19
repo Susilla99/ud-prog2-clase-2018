f577ecd9161308dfa7f7d35b779d2dca840b8ae8
Andoni Eguiluz <andoni.eguiluz@deusto.es>
2018-04-19 20:31:34 +0200

    Clase ventanas

--- Clase2018Prog2/src/tema07/VentanaUsuario.java
+++ Clase2018Prog2/src/tema07/VentanaUsuario.java
@@ -8,8 +8,27 @@ public class VentanaUsuario extends JFrame {
 		setTitle( "Usuario 1" );
 		setSize( 320, 200 );
 		setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+		setIconImage( new ImageIcon( getClass().getResource("UD-icon.png" )).getImage() );  // Cambiar el icono de la ventana (ojo - tiene que tener el tamaño apropiado)
 		// 2.- Crear sus componentes y contenedores
+		JPanel pInferior = new JPanel();
+		JButton bCancelar = new JButton( "Cancelar" );
+		JButton bAceptar = new JButton( "Aceptar" );
+		JLabel lFoto = new JLabel( new ImageIcon( "src/tema07/coche.png" ) );
+		JTextField tfNick = new JTextField( "nick", 10 );
+		JTextArea taCV = new JTextArea( 5, 20 );
+		// 2b.- Definir layouts
+		this.setLayout( new BorderLayout() );  // Por defecto es así
+		pInferior.setLayout( new FlowLayout() ); // Por defecto es así
+		// 2c.- Confirgurar componentes
+		lFoto.setSize( 100, 50 );
 		// 3.- Asociar componentes a contenedores
+		this.add( tfNick, BorderLayout.WEST );
+		JScrollPane spCV = new JScrollPane( taCV );
+		this.add( spCV , BorderLayout.CENTER );
+		this.add( lFoto, BorderLayout.EAST );
+		this.add( pInferior, BorderLayout.SOUTH );
+		pInferior.add( bCancelar );
+		pInferior.add( bAceptar );
 		// 4.- Crear y asociar eventos a componentes
 	}
 
